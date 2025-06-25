ac8d7c737fe3397231386b81b3f7a0c2fe380733
rahul <rkrajput0941@gmail.com>
2025-06-25 16:25:47 +0530

    instance type change

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # Security Group
 resource "aws_security_group" "web_sg" {
   name        = "PortfolioDevOps_sg"
-  description = "Allow ports 22, 80, 443"
+  description = "Allow ports 22, 80, 443, 30000-32767"
   vpc_id      = "vpc-0c2724df46c58da55"  # your VPC ID
 
   ingress {
@@ -51,7 +51,7 @@ resource "aws_security_group" "web_sg" {
 # EC2 Instance
 resource "aws_instance" "web_server" {
   ami                         = "ami-0e83aa1f9d9f0d3f8"
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   key_name                    = var.key_name
   subnet_id                   = "subnet-02e02b36f728c617a"        # your Subnet ID
   vpc_security_group_ids      = [aws_security_group.web_sg.id]
